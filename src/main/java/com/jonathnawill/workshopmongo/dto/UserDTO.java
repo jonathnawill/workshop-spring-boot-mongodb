28da9dcd8d8265d44d7d3dba121208a9b8a50284
Jonathan William <Jjoninha11@hotmail.com>
2023-08-17 15:28:10 -0300

    Obtendo um usuário por id

--- src/main/java/com/jonathnawill/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/jonathnawill/workshopmongo/dto/UserDTO.java
@@ -1,18 +1,23 @@
 package com.jonathnawill.workshopmongo.dto;
 
+import java.io.Serializable;
+
 import com.jonathnawill.workshopmongo.domain.User;
 
-public class UserDTO {
 
+public class UserDTO implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
 	private String id;
 	private String name;
 	private String email;
 
-	
 	public UserDTO() {
-		
+
 	}
-	
+
 	public UserDTO(User obj) {
 		id = obj.getId();
 		name = obj.getName();
@@ -42,6 +47,5 @@ public class UserDTO {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
+
 }
