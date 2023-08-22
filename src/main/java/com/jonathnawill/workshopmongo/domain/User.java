40f0bb01b62a3dd1eb85a89d3053b1e1136c8d3e
Jonathan William <Jjoninha11@hotmail.com>
2023-08-22 13:05:19 -0300

    Referenciando os posts do usuario

--- src/main/java/com/jonathnawill/workshopmongo/domain/User.java
+++ src/main/java/com/jonathnawill/workshopmongo/domain/User.java
@@ -1,21 +1,27 @@
 package com.jonathnawill.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 
 @Document(collection = "user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
+	
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 
 	public User() {
 
@@ -66,6 +72,10 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	public List<Post> getPosts() {
+		return posts;
 	}
 
 }
