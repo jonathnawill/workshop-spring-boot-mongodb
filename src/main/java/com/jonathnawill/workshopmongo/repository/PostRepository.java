fe62ce85e412c62d523aa63d13e0b460cdbcadb9
Jonathan William <Jjoninha11@hotmail.com>
2023-08-22 14:09:51 -0300

    Consulta simples com query methods

--- src/main/java/com/jonathnawill/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jonathnawill/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.jonathnawill.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.jonathnawill.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
